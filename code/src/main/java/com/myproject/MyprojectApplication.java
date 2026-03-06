d422292c1da8ff9ba0934ee7fd82918250c35884
loginkarun <loginkarun@gmail.com>
2026-03-06 15:15:52 +0530

    Auto commit: Scripts committed on 2026-03-06-09-45-51

---
+++ code/src/main/java/com/myproject/MyprojectApplication.java
@@ -0,0 +1,18 @@
+package com.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Application class for Shopping Cart API
+ * 
+ * @author Generated
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class MyprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyprojectApplication.class, args);
+    }
+}
