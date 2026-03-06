72a3e7abb0203b968924849248f3431891fea4b5
loginkarun <loginkarun@gmail.com>
2026-03-06 15:16:47 +0530

    Auto commit: Scripts committed on 2026-03-06-09-46-46

---
+++ code/src/main/java/com/myproject/models/repositories/CartRepository.java
@@ -0,0 +1,35 @@
+package com.myproject.models.repositories;
+
+import com.myproject.models.entities.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Repository interface for Cart entity
+ */
+@Repository
+public interface CartRepository extends JpaRepository<Cart, UUID> {
+
+    /**
+     * Find cart by user ID
+     */
+    Optional<Cart> findByUserId(UUID userId);
+
+    /**
+     * Find cart by session ID
+     */
+    Optional<Cart> findBySessionId(String sessionId);
+
+    /**
+     * Delete cart by user ID
+     */
+    void deleteByUserId(UUID userId);
+
+    /**
+     * Delete cart by session ID
+     */
+    void deleteBySessionId(String sessionId);
+}
